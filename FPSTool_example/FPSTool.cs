4660339e5adff08d5bfac359e80ed3daa58a012e
HowardCheng <makubex49@gmail.com>
2020-12-17 14:42:32 +0800

    rename variable

--- FPSTool_example/FPSTool.cs
+++ FPSTool_example/FPSTool.cs
@@ -4,35 +4,40 @@ namespace AIM.Modules
 {
     class FPSTool
     {
-        int fps1_count = 0;
-        int fps1 = 0;
-        DateTime last_fps_time1 = DateTime.Now;
+        #region fixed time frame number method
+        int fps_count = 0;
+        int fps_temp = 0;
+        DateTime last_fps_update_time = DateTime.Now;
         public double FPS()
         {
-            this.fps1_count++;
+            this.fps_count++;
 
             var nowtime = DateTime.Now;
-            var sp = nowtime - this.last_fps_time1;
+            var sp = nowtime - this.last_fps_update_time;
             if (sp.TotalSeconds >= 1.0)
             {
-                this.fps1 = this.fps1_count;
+                this.fps_temp = this.fps_count;
 
-                this.last_fps_time1 = nowtime;
-                this.fps1_count = 0;
+                this.last_fps_update_time = nowtime;
+                this.fps_count = 0;
             }
 
-            return this.fps1;
+            return this.fps_temp;
         }
+        #endregion
 
+        #region the average sampling time method
         double avg_duration = 0.0;
-        DateTime last_fps2_time = DateTime.Now;
+        DateTime last_fps_check_time = DateTime.Now;
         public double FPS(int avg_num)
         {
-            TimeSpan sp = DateTime.Now - this.last_fps2_time;
-            this.last_fps2_time = DateTime.Now;
+            TimeSpan sp = DateTime.Now - this.last_fps_check_time;
+            this.last_fps_check_time = DateTime.Now;
             this.avg_duration -= this.avg_duration / avg_num;
             this.avg_duration += sp.TotalSeconds / avg_num;
             return 1.0 / this.avg_duration;
         }
+        #endregion
+
     }
 }
