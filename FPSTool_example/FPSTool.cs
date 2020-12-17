14d13f4873650c676a9a14002a6bf5d148cbfabd
HowardCheng <makubex49@gmail.com>
2020-12-17 13:41:24 +0800

    init

---
+++ FPSTool_example/FPSTool.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIM.Modules
+{
+    class FPSTool
+    {
+        int fps_count = 0;
+        int fps = 0;
+        DateTime last_fps_time1 = DateTime.Now;       
+        public double FPS()
+        {
+            this.fps_count++;
+
+            var nowtime = DateTime.Now;
+            var sp = nowtime - this.last_fps_time1;
+            if (sp.TotalSeconds >= 1.0) 
+            {
+                this.fps = this.fps_count;
+
+                this.last_fps_time1 = nowtime;
+                this.fps_count = 0;
+            }
+
+            return this.fps;
+        }
+
+        double fps_avg = 0.0;
+        DateTime last_fps_time2 = DateTime.Now;
+        public double FPS(int N)
+        {
+            TimeSpan sp = DateTime.Now - this.last_fps_time2;
+            double fps = 1.0 / sp.TotalSeconds;
+
+            // https://stackoverflow.com/questions/10990618/calculate-rolling-moving-average-in-c
+            this.fps_avg -= this.fps_avg / N;
+            this.fps_avg += fps / N;
+
+            this.last_fps_time1 = DateTime.Now;
+            return this.fps_avg;
+        }
+    }
---
+++ FPSTool_example/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FPSTool_example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.trackBar1.Value = 50;
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            this.Text = $"timer.Interval = {trackBar1.Value}";
+            this.timer1.Interval = trackBar1.Value;
+            this.timer2.Interval = trackBar1.Value;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
